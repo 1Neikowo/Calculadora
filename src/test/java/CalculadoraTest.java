84f7f317e374688bcb463172b87188eb975d484a
sbeae <130028710+sbeae@users.noreply.github.com>
2023-09-21 19:01:26 -0300

    CalculadoraTest.java
    Tests cuadratica

--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -1,5 +1,39 @@
 import static org.junit.jupiter.api.Assertions.*;
+package org.example;
+
+import org.example.Cuadratica;
+import org.junit.jupiter.api.Test;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import static org.junit.jupiter.api.Assertions.*;
 
 class CalculadoraTest {
+
+    @Test
+    public void ingresarAValidoTest() {
+        // Simular entrada de usuario (por ejemplo, "2.0\n")
+        String input = "hola\n";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        // Ejecutar el método y verificar si devuelve el valor correcto
+        double a = Cuadratica.ingresarA(0.0);
+        assertEquals(2.0, a, 0.0001); // Verifica que el valor ingresado sea igual a 2.0
+
+        // Restaurar la entrada estándar original
+        System.setIn(System.in);
+    }
+    @Test
+    public void calcularCuadratica1Test() {
+        double x1 = Cuadratica.calcularCuadratica1(0, 1.0, -3.0, 2.0, 1.0);
+        assertEquals(2.0, x1, 0.0001); // Verifica que el cálculo de la primera raíz sea correcto
+    }
+
+    @Test
+    public void calcularCuadratica2Test() {
+        double x2 = Cuadratica.calcularCuadratica2(0, 1.0, -3.0, 2.0, 1.0);
+        assertEquals(1.0, x2, 0.0001); // Verifica que el cálculo de la segunda raíz sea correcto
+    }
+
 
 }
