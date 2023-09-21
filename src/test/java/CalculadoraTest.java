624a7fba49d288c57008f99443795329db1c9ca9
1Neikowo <71148894+1Neikowo@users.noreply.github.com>
2023-09-21 20:07:49 -0300

    Merge Figuras y Calculo recta

--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -80,21 +80,6 @@ class CalculadoraTest {
         assertNotEquals(10, Calculadora.volumenCono(3,3));
     }
 
-
-    @Test
-    public void ingresarAValidoTest() {
-        // Simular entrada de usuario (por ejemplo, "2.0\n")
-        String input = "hola\n";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-
-        // Ejecutar el método y verificar si devuelve el valor correcto
-        double a = Calculadora.ingresarA(0.0);
-        assertEquals(2.0, a, 0.0001); // Verifica que el valor ingresado sea igual a 2.0
-
-        // Restaurar la entrada estándar original
-        System.setIn(System.in);
-    }
     @Test
     public void calcularCuadratica1Test() {
         double x1 = Calculadora.calcularCuadratica1(0, 1.0, -3.0, 2.0, 1.0);
